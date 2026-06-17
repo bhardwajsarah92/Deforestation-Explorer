c4cea0d4d3c9c4ed6b4f20fe626b419c4430e6bd
bhardwajsarah92 <bhardwajsarah92@gmail.com>
2026-06-17 19:29:46 -0400

    app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title("Deforestation Explorer") 
+st.write("Welcome to the interactive data explorer")
+st.write("This website exmains deforestation patterns across the world, how they have changed over time, and what causes them.")
+st.write("Feel free to check out the Next Steps Section to see how you can do your part in helping the climate.")
